f25bf3886bd18a530ebb40fe9a58aaee544623c0
Adaamss <aademhamdi@gmail.com>
2026-05-20 14:45:50 +0100

    feat: apply validation middleware to create flight route

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createFlight } from "../controller/flight.controller";
+import { validateCreateFlight } from "../middleware/flight.middleware";
 
 export const flightsRouter = express.Router();
 
-flightsRouter.post("/", createFlight); 
+flightsRouter.post("/", validateCreateFlight, createFlight);
