29be38eb24cf6506658fe0c744b5e4df2e2e4313
Adaamss <aademhamdi@gmail.com>
2026-05-20 14:47:25 +0100

    feat: add 409 response for duplicate flight number

--- src/controller/flight.controller.ts
+++ src/controller/flight.controller.ts
@@ -3,7 +3,10 @@ import { createFlightService } from "../service/flightService";
 import { FlightBody } from "../types/flight.types";
 
 export const createFlight = async (req: Request, res: Response) => {
-  const result = await createFlightService(req.body as FlightBody);
-
-  return res.status(200).json({ flight: result });
+  try {
+    const result = await createFlightService(req.body as FlightBody);
+    return res.status(201).json({ flight: result });
+  } catch (error) {
+    return res.status(409).json({ message: (error as Error).message });
+  }
 };
