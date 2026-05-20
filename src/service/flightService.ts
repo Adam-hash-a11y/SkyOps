89e7b709dc06ecfda786ebf7b10a7cbe7e76a784
Adaamss <aademhamdi@gmail.com>
2026-05-20 14:46:04 +0100

    feat: check for existing flight before creation

--- src/service/flightService.ts
+++ src/service/flightService.ts
@@ -1,6 +1,13 @@
-import { saveFlight } from "../repository/flight.repository";
+import {
+  findFlightByNumber,
+  saveFlight,
+} from "../repository/flight.repository";
 import { FlightBody } from "../types/flight.types";
 
 export const createFlightService = async (flight: FlightBody) => {
+  const isExistingFlight = await findFlightByNumber(flight.flightNumber);
+  if (isExistingFlight) {
+    throw new Error("flight number already exists");
+  }
   return await saveFlight(flight);
 };
