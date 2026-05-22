dd82cd58da8578efc03ea645c4d6b739aab0c27e
Adaamss <aademhamdi@gmail.com>
2026-05-22 22:43:47 +0100

    feat: add GET routes for flights with middleware

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -1,7 +1,14 @@
 import express from "express";
-import { createFlight } from "../controller/flight.controller";
+import {
+  createFlight,
+  getFlight,
+  getFlights,
+} from "../controller/flight.controller";
 import { validateCreateFlight } from "../middleware/flight.middleware";
+import { validateGetFlights } from "../middleware/validateGetFlights.middleware";
 
 export const flightsRouter = express.Router();
 
 flightsRouter.post("/", validateCreateFlight, createFlight);
+flightsRouter.get("/", validateGetFlights, getFlights);
+flightsRouter.get("/:flightNumber", getFlight);
