30ad5351045300fb3738060fa5ec3b87127e101a
Adaamss <aademhamdi@gmail.com>
2026-05-22 22:43:06 +0100

    feat: add getFlights and getFlight controllers

--- src/controller/flight.controller.ts
+++ src/controller/flight.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
-import { createFlightService } from "../service/flightService";
-import { FlightBody } from "../types/flight.types";
+import {
+  createFlightService,
+  getFlightByFlightNumber,
+  getFlightsByFilters,
+} from "../service/flightService";
+import { FlightBody, FlightStatus } from "../types/flight.types";
 
 export const createFlight = async (req: Request, res: Response) => {
   try {
@@ -9,4 +13,28 @@ export const createFlight = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(409).json({ message: (error as Error).message });
   }
+};
+
+export const getFlight = async (
+  req: Request<{ flightNumber: string }>,
+  res: Response,
+) => {
+  const result = await getFlightByFlightNumber(req.params.flightNumber);
+  if (!result) {
+    return res.status(404).json({ message: "flight not found" });
+  }
+  return res.status(200).json(result);
+};
+
+export const getFlights = async (req: Request, res: Response) => {
+  const { status, origin, destination, sortBy } = req.query;
+
+  const result = await getFlightsByFilters({
+    origin: origin as string,
+    destination: destination as string,
+    status: status as FlightStatus,
+    sortBy: sortBy as string,
+  });
+
+  return res.status(200).json({ flights: result });
 };
