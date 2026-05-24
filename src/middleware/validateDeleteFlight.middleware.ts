d98ebc25a9c7c3629b3a726bd9a7af250c811b69
Adaamss <aademhamdi@gmail.com>
2026-05-24 23:45:35 +0100

    feat: add delete flight validation middleware

---
+++ src/middleware/validateDeleteFlight.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { isValidFlightNumber } from "../validator/flight.validator";
+
+export const validateDeleteFlight = (
+  req: Request<{ flightNumber: string }>,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (!isValidFlightNumber(req.params.flightNumber)) {
+    res
+      .status(400)
+      .json({
+        message:
+          "flight number must start with SKYOPS- followed by at least one character",
+      });
+    return;
+  }
+  next();
+};
