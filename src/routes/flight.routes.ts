11cb24ee9d914c86e313c4c370f23440b19ccc0f
Adaamss <aademhamdi@gmail.com>
2026-05-24 23:45:53 +0100

    feat: add DELETE route for flight by flight number

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import {
   createFlight,
+  deleteFlight,
   getFlight,
   getFlights,
 } from "../controller/flight.controller";
 import { validateCreateFlight } from "../middleware/flight.middleware";
 import { validateGetFlights } from "../middleware/validateGetFlights.middleware";
+import { validateDeleteFlight } from "../middleware/validateDeleteFlight.middleware";
 
 export const flightsRouter = express.Router();
 
 flightsRouter.post("/", validateCreateFlight, createFlight);
 flightsRouter.get("/", validateGetFlights, getFlights);
 flightsRouter.get("/:flightNumber", getFlight);
+flightsRouter.delete("/:flightNumber", validateDeleteFlight, deleteFlight);
