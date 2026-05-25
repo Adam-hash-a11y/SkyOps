7d87d6098787785ebbd4040bd337390855d7ea9f
Adaamss <aademhamdi@gmail.com>
2026-05-25 16:15:51 +0100

    feat: add PATCH /:flightNumber route with validateFlightNumber and validateUpdateFlight middleware functions

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -4,14 +4,22 @@ import {
   deleteFlight,
   getFlight,
   getFlights,
+  updateFlight,
 } from "../controller/flight.controller";
 import { validateCreateFlight } from "../middleware/flight.middleware";
 import { validateGetFlights } from "../middleware/validateGetFlights.middleware";
-import { validateDeleteFlight } from "../middleware/validateDeleteFlight.middleware";
+import { validateFlightNumber } from "../middleware/validateFlightNumber.middleware";
+import { validateUpdateFlight } from "../middleware/validateUpdateFlight.middleware";
 
 export const flightsRouter = express.Router();
 
 flightsRouter.post("/", validateCreateFlight, createFlight);
 flightsRouter.get("/", validateGetFlights, getFlights);
 flightsRouter.get("/:flightNumber", getFlight);
-flightsRouter.delete("/:flightNumber", validateDeleteFlight, deleteFlight);
+flightsRouter.delete("/:flightNumber", validateFlightNumber, deleteFlight);
+flightsRouter.patch(
+  "/:flightNumber",
+  validateFlightNumber,
+  validateUpdateFlight,
+  updateFlight,
+);
