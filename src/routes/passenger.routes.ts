93220f2a911b61ebfba6171376b24b85c444f857
Adaamss <aademhamdi@gmail.com>
2026-05-27 16:31:27 +0100

    feat: add GET route for passenger by passport number

--- src/routes/passenger.routes.ts
+++ src/routes/passenger.routes.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { createPassenger } from "../controller/passenger.controller";
+import {
+  createPassenger,
+  getPassenger,
+} from "../controller/passenger.controller";
 import { validateCreatePassenger } from "../middleware/ValidatePassengerCreation.middleware";
+import { validatePassportNumber } from "../middleware/validatePassportNumber.middleware";
 
 export const passengersRouter = express.Router();
 
 passengersRouter.post("/", validateCreatePassenger, createPassenger);
+passengersRouter.get("/:passportNumber", validatePassportNumber, getPassenger);
