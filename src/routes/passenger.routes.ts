7399fa995273e4e088eb412f9986116809a9ed03
Adaamss <aademhamdi@gmail.com>
2026-05-28 20:36:52 +0100

    feat: add GET passengers route with filters

--- src/routes/passenger.routes.ts
+++ src/routes/passenger.routes.ts
@@ -3,13 +3,16 @@ import {
   createPassenger,
   deletePassenger,
   getPassenger,
+  getPassengers,
 } from "../controller/passenger.controller";
 import { validateCreatePassenger } from "../middleware/ValidatePassengerCreation.middleware";
 import { validatePassportNumber } from "../middleware/validatePassportNumber.middleware";
+import { validateGetPassengers } from "../middleware/validateGetPassengers.middleware";
 
 export const passengersRouter = express.Router();
 
 passengersRouter.post("/", validateCreatePassenger, createPassenger);
+passengersRouter.get("/", validateGetPassengers, getPassengers);
 passengersRouter.get("/:passportNumber", validatePassportNumber, getPassenger);
 passengersRouter.delete(
   "/:passportNumber",
